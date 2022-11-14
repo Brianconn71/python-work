3c033232808d58812d036d2479c1d27ea1f04ae7
brianconn71 <brian.connolly>
2022-11-14 21:12:23 +0000

    week8 done

--- week8/get_frequencies.py
+++ week8/get_frequencies.py
@@ -1,4 +1,4 @@
-file = '/home/briancon71/scripting/week8/alice.txt'
+file = '/home/briancon71/python-work/week8/wordlist.txt'
 
 
 def get_frequencies(file_name):
@@ -13,18 +13,18 @@ def get_frequencies(file_name):
         
         #convert to lowercase
         lower_case_data = data.lower()
+
+        #only english language alphabet characters
+        alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",]
         
        # for loop
         for letter in lower_case_data:
-            if letter.isalpha():
-                data.count(letter)
+            if letter.lower() in alphabet:
                 new_dict[letter]= new_dict.get(letter,0) +1
                 
         # new_dict = [new_dict.get(letter,0) +1 for letter in lower_case_data if letter.isalpha()]
         return new_dict
 
-	
-# alice.txt
 frequencies = get_frequencies(file)
 print("Letter Frequency")
 for letter, frequency  in sorted(frequencies.items()):
